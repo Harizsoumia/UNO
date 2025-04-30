a678b50449664aee6b25d333fb209a3cad90a1cc
Harizsoumia <155814890+Harizsoumia@users.noreply.github.com>
2025-04-30 19:39:03 +0200

    Update Deck.java

--- src/unoGamepackage/Deck.java
+++ src/unoGamepackage/Deck.java
@@ -1,64 +1,89 @@
 package unoGamepackage;
-
+import java.awt.Color;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Deck {
-    private List<Card> cards;
+    private final List<Card> cards;
 
+    /**
+     * Constructor that initializes a standard UNO deck
+     */
     public Deck() {
         cards = new ArrayList<>();
-        initializeDeck();
+        initializeCards();
         shuffle();
     }
 
-    private void initializeDeck() {
-    	String[] colors = {"Red", "Yellow", "Green", "Blue"};
-    	String[] numbers = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
-    	String[] actions = {"skip", "reverse", "draw 2"};
+    /**
+     * Initializes the deck with standard UNO cards
+     */
+    private void initializeCards() {
+        // Colors: Red, Blue, Green, Yellow
+        Color[] colors = {Color.RED, Color.BLUE, Color.GREEN, Color.YELLOW};
 
+        for (Color color : colors) {
+            // Add one 0 card
+            cards.add(new NumberCard(color, 0));
 
-        // Cartes numérotées
-        for (String color : colors) {
-            // Une carte 0 par couleur
-            cards.add(new Card (color, "0"));
-            // Deux cartes de chaque nombre de 1 à 9 par couleur
+            // Add two of each number cards 1-9
             for (int i = 1; i <= 9; i++) {
-                cards.add(new Card(color, String.valueOf(i)));
-                cards.add(new Card(color, String.valueOf(i)));
+                cards.add(new NumberCard(color, i));
+                cards.add(new NumberCard(color, i));
+            }
+
+            // Add two of each action cards (Skip, Reverse, Draw Two)
+            for (int i = 0; i < 2; i++) {
+                cards.add(new SkipCard(color));
+                cards.add(new ReverseCard(color));
+                cards.add(new DrawTwoCard(color));
             }
         }
 
-        // Cartes spéciales
-        for (String color : colors) {
-            for (String action : actions) {
-                // Deux cartes de chaque action par couleur
-                cards.add(new Card(color, action));
-                cards.add(new Card(color, action));
-            }
-        }
-
-        // Cartes jokers
+        // Add four wild cards and four wild draw four cards
         for (int i = 0; i < 4; i++) {
-            cards.add(new Card("Wild", "Wild"));
-            cards.add(new Card("Wild", "Wild_draw_4"));
+            cards.add(new WildCard());
+            cards.add(new WildDrawFourCard());
         }
     }
 
+    /**
+     * Shuffles the deck
+     */
     public void shuffle() {
         Collections.shuffle(cards);
     }
 
+    /**
+     * Draws a card from the deck
+     * @return The drawn card
+     */
     public Card drawCard() {
-        if (!cards.isEmpty()) {
-            return cards.remove(0);
-        } else {
-            return null; // Le deck est vide
+        if (cards.isEmpty()) {
+            // If deck is empty, reshuffle the discard pile
+            // In a real implementation, we'd need to get cards from the discard pile
+            // For simplicity, we're creating a new deck
+            initializeCards();
+            shuffle();
         }
+
+        return cards.remove(0);
     }
 
-    public List<Card> getCards() {
-        return cards;
+    /**
+     * Adds a card to the bottom of the deck
+     * @param card The card to add
+     */
+    public void addCard(Card card) {
+        cards.add(card);
     }
-}
+
+    /**
+     * Gets the number of cards in the deck
+     * @return The number of cards
+     */
+    public int getSize() {
+        return cards.size();
+    }
+} 
